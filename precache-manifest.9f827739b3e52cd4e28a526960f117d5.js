a77af16b36df8ac31588955b9e6c74333c1f9678
Nikita864 <nikitavashisht864@gmail.com>
2020-05-07 10:59:59 +0530

    Updates

---
+++ precache-manifest.9f827739b3e52cd4e28a526960f117d5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a965bd48a64d30fd2ef",
+    "url": "/Rent-A-Car/static/css/main.9f37560c.chunk.css"
+  },
+  {
+    "revision": "9a965bd48a64d30fd2ef",
+    "url": "/Rent-A-Car/static/js/main.9a965bd4.chunk.js"
+  },
+  {
+    "revision": "c2e348da977f31eec2d5",
+    "url": "/Rent-A-Car/static/js/1.c2e348da.chunk.js"
+  },
+  {
+    "revision": "a2dc11f2892fadb233f8",
+    "url": "/Rent-A-Car/static/js/runtime~main.a2dc11f2.js"
+  },
+  {
+    "revision": "c68d60971b3343ca1bb8796c4575b786",
+    "url": "/Rent-A-Car/static/media/audi.c68d6097.jpg"
+  },
+  {
+    "revision": "07c97e4335f2c0d7e8c142e364d1f044",
+    "url": "/Rent-A-Car/static/media/bmw.07c97e43.jpg"
+  },
+  {
+    "revision": "d43a35972e9cc13fbd9077a09903be21",
+    "url": "/Rent-A-Car/static/media/modal.d43a3597.jpg"
+  },
+  {
+    "revision": "ebc1784ce60a8b825d0c433c2ed0b031",
+    "url": "/Rent-A-Car/static/media/merc.ebc1784c.jpg"
+  },
+  {
+    "revision": "4749f91e1ad3aad39aec052dd5614517",
+    "url": "/Rent-A-Car/static/media/min.4749f91e.jpg"
+  },
+  {
+    "revision": "a5ecd376626524e2cd9b1fc8737a176b",
+    "url": "/Rent-A-Car/static/media/retro.a5ecd376.jpg"
+  },
+  {
+    "revision": "a4e11dda40531debd374e4c8b1dcc7f4",
+    "url": "/Rent-A-Car/static/media/Poppins-Medium.a4e11dda.ttf"
+  },
+  {
+    "revision": "731a28a413d642522667a2de8681ff35",
+    "url": "/Rent-A-Car/static/media/Poppins-Regular.731a28a4.ttf"
+  },
+  {
+    "revision": "e63b93dfac2600782654e2b87910d681",
+    "url": "/Rent-A-Car/static/media/Poppins-SemiBold.e63b93df.ttf"
+  },
+  {
+    "revision": "7940efc40d8e3b477e16cc41b0287139",
+    "url": "/Rent-A-Car/static/media/Poppins-Bold.7940efc4.ttf"
+  },
+  {
+    "revision": "c88cecbffad6d8e731fd95de49561ebd",
+    "url": "/Rent-A-Car/static/media/Montserrat-SemiBold.c88cecbf.ttf"
+  },
+  {
+    "revision": "88932dadc42e1bba93b21a76de60ef7a",
+    "url": "/Rent-A-Car/static/media/Montserrat-Bold.88932dad.ttf"
+  },
+  {
+    "revision": "9c46095118380d38f12e67c916b427f9",
+    "url": "/Rent-A-Car/static/media/Montserrat-Regular.9c460951.ttf"
+  },
+  {
+    "revision": "0fe351143b000b9fdbd37603a5099dc7",
+    "url": "/Rent-A-Car/index.html"
+  }
+];
